9a66b39fcbaf2587e316ecabbcf10b9a64be9ab6
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-08-01 08:16:15 -0700

    Implement linked-list addToTail

---
+++ linked-list.js
@@ -0,0 +1,19 @@
+class LinkedList() {
+  constructor() {
+    this.head = this.tail = null;
+  }
+
+  addToTail(value) {
+    let node = {
+      value: value,
+      next: null,
+    }
+
+    if (this.head === this.tail === null) {
+      this.head = this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+}
