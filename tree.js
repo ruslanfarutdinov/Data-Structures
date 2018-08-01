7b83ade1c5f5abd2d736ce48a0c512791863f091
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-08-01 11:17:03 -0700

    Add tree time complexity analysis

--- tree.js
+++ tree.js
@@ -7,6 +7,7 @@ class Tree {
   addChild(value) {
     const newTree = new Tree(value);
     this.children.push(newTree);
+    return newTree;
   }
 
   contains(value) {
@@ -23,3 +24,7 @@ class Tree {
     return false;
   }
 }
+
+// Time complexity of the above methods?
+// addChild - O(1), note that pushing into arrays is also O(1)
+// contains - O(n), since the algorithm visits each node, looking for the value
