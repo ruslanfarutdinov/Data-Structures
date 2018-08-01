48161ff89ffb8629d3100768e4860afd6fba4b9a
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-08-01 10:36:34 -0700

    Implement tree contains()

--- tree.js
+++ tree.js
@@ -9,4 +9,17 @@ class Tree {
     this.children.push(newTree);
   }
 
+  contains(value) {
+    if (this.value === value) {
+      return true;
+    }
+
+    for (let i = 0; i < this.children.length; i += 1) {
+      const child = this.children[i];
+      const found = child.contains(value);
+      if (found) return true;
+    }
+
+    return false;
+  }
 }
