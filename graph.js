31bc2799b0f28e6603088491a29919efeea45147
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-08-02 12:19:54 -0700

    Implement Graph removeNode()

--- graph.js
+++ graph.js
@@ -18,4 +18,14 @@ class Graph {
     }
     return false;
   }
+
+  removeNode(value) {
+    for (let i = 0; i < this.storage.length; i += 1) {
+      const node = this.storage[i];
+      if (node.value === value) {
+        this.storage.splice(i, 1);
+        return;
+      }
+    }
+  }
 }
