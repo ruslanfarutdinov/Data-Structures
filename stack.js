ad978b001b0f1a3ab9128f3e0f65d9297aacb104
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-08-09 09:57:00 -0700

    Implement stack

---
+++ stack.js
@@ -0,0 +1,18 @@
+class Stack {
+  constructor() {
+    this.size = 0;
+    this.position = 0;
+  }
+
+  push(str) {
+    this[this.position] = str;
+    this.size += 1;
+    this.position += 1;
+  }
+
+  pop() {
+    this.position -= 1;
+    this.size -= 1;
+    delete this[this.position];
+  }
+}
