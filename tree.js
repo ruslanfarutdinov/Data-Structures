5c929327162d90d66ff79a037b251696a93dfdb2
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-08-23 10:51:04 -0700

    Add Breadth-First Search to Tree

--- tree.js
+++ tree.js
@@ -11,6 +11,7 @@ class Tree {
   }
   // addChild - O(1), note that pushing into arrays is also O(1)
 
+  // This contains algorithm is implemented via Depth-First Search
   contains(value) {
     if (this.value === value) {
       return true;
@@ -22,6 +23,27 @@ class Tree {
       if (found) return true;
     }
 
+    return false;
+  }
+
+  // This contains algorithm is implemented via Breadth-First Search
+  containsBFS(value) {
+    if (this.value === value) return true;
+
+    const treesWithChildren = [this];
+
+    for (let i = 0; i < treesWithChildren.length; i += 1) {
+      const tree = treesWithChildren[i];
+      for (let j = 0; j < tree.children.length; j += 1) {
+        const child = tree.children[j];
+        if (child.value === value) {
+          return true;
+        }
+        if (child.children.length > 0) {
+          treesWithChildren.push(child);
+        }
+      }
+    }
     return false;
   }
 }
